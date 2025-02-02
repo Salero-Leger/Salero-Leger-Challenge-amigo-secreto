de5a4de493edaf73b5eb6165ecf7aa98b6a19f82
Salero-Leger <salero.leger@gmail.com>
2025-02-02 12:07:04 -0400

    Version final 3.0

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 // Creamos un var con el array  AmigosSecretosAgregados
 var AmigosSecretosAgregados = [];
 
+
+
 function limpiardatos(){
     //Funcion para dejar caja vacia despues de capturar datos
     let limpiarCaja = document.getElementById("amigo");
     limpiarCaja.value = "";
 }
 function actualizarListaAmigos(){
+    //Obtener el ul
+    //Asegurar que la lista este vacia para agregar
     const lista = document.getElementById("listaAmigos");
      lista.innerHTML = ""
+     //iteracion que crea listas 
     for (let i =0; i < AmigosSecretosAgregados.length; i++){
         let li =document.createElement("li");
         li.textContent = AmigosSecretosAgregados[i];
@@ -29,4 +34,20 @@ function agregarAmigo(){
     }
     limpiardatos();
     console.log (AmigosSecretosAgregados);
+}
+//Función sortear  entre los amigos agregados
+function sortearAmigo(){
+   
+    // Comprobar si hay datos en el array
+    if (AmigosSecretosAgregados.length === 0){
+        alert("Por favor, inserte primero un dato.");
+    }
+    //Generar indice aleatorio
+    let indiceAleatorio = Math.floor(Math.random()*AmigosSecretosAgregados.length);
+    //Llamar al contenido del array con el numero aleatorio generado
+    let nombreGanadorSorteo = AmigosSecretosAgregados[indiceAleatorio];
+    //Mostrar resultado
+    const nombreMostar = document.getElementById("resultado");
+    nombreMostar.innerHTML = `Felicidades ganaste: ${nombreGanadorSorteo}`;
+
 }
